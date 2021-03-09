253f306b68ed8bb3e39e9b57fc146a2de6f33f35
Jean Lionel <nijeanlionel@gmail.com>
2021-03-09 05:13:51 +0200

    add Annuaire telephonique

---
+++ resources/views/site/annuaire.blade.php
@@ -0,0 +1,13 @@
+@include('site.header')
+
+<div class="container">
+
+	<div class="embed-responsive">
+
+		<iframe src="{{ asset('/doc/annuaire%20telephonique.pdf') }}" width="100%" height="800px">
+		
+	</div>
+	
+</div>
+
+@include('site.footer')
