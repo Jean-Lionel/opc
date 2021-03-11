1f3250560babf639ed9f6a07d182f25eb1188c08
Jean Lionel <nijeanlionel@gmail.com>
2021-03-11 16:28:39 +0200

    Personne Live wire

--- resources/views/livewire/person-livewire.blade.php
+++ resources/views/livewire/person-livewire.blade.php
@@ -130,4 +130,67 @@
 		
 
 	</div>
+
+
+
+	<div class="">
+        <div class="row">
+            <div class="col-md-9">
+
+                <table class="table  table-responsive">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>No d'ordre</th>
+                        <th>Nom et Prénom</th>
+                        <th>Numéro</th>
+                        {{-- <th>Téléphone</th>
+                        <th>TABLEAU</th>
+                        <th>TABLEAU</th>
+                        <th>NIF</th>
+                        <th>TYPE</th> --}}
+                        <th>Action</th>
+                    </tr>
+                    
+                </thead>
+
+                <tbody>
+                    @foreach($personnes as $key => $personne)
+                      <tr>
+                        <td>{{ ++$key }}</td>
+                        <td>{{ $personne->order_number }}</td>
+                        <td>{{ $personne->first_name }}</td>
+                        <td>{{ $personne->compte->name ?? "" }}</td>
+                       {{--  <td>{{ $personne->telephone }}</td>
+                        <td>{{ $personne->table_name }}</td>
+                        <td>{{ $personne->nif }}</td>
+                        <td>{{ $personne->addresse }}</td>
+                        <td>{{ $personne->type_personne }}</td> --}}
+                        <td class="d-flex justify-content-around">
+                            <button><i class="fa fa-eye"></i></button>
+                      <button class="btn-sm btn-info" wire:click="modifierPersonne({{ $personne->id }})" title="Modifier"><i class="fa fa-edit"></i></button>
+
+                       @if($personne->valider !='VALIDER')
+
+                      <button class="btn btn-info" wire:click="validerPersonner({{ $personne->id  }})"><i class="fa fa-check"></i></button>
+                        @endif
+                            <button class="btn-sm btn-danger ml-3" wire:click="supprimerPersonne({{ $personne->id }})" title="Supprimer"><i class="fa fa-trash-o"></i></button>
+                        </td>
+                      </tr>
+
+                    @endforeach
+                </tbody>
+            </table>
+
+          {{ $personnes->links() }}
+        
+
+            </div>
+
+        <div class="col-md-3">
+            12
+        </div>
+        </div>
+		
+	</div>
 </div>
